3381d4b7b6f6e272c722266e35f2fc802e283ae6
LeoSanta183 <santazleo2004@gmail.com>
2024-04-12 11:51:34 -0400

    Spawners work in forest, just need correct references

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    public Transform[] spawnPoints;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 0.5f, 1f);
+    }
+
+    void SpawnEnemy()
+    {
+        if (spawnPoints.Length == 0 || enemyPrefabs.Length == 0)
+        {
+            Debug.LogWarning("Spawn points or asteroid prefabs are not set!");
+            return;
+        }
+
+        int randomEnemy = Random.Range(0, enemyPrefabs.Length);
+        int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(enemyPrefabs[randomEnemy], spawnPoints[randomSpawnPoint].position, Quaternion.Euler(0, 0, 180));
+    }
+}

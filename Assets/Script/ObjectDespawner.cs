064db590bed41aaa3fef3b4338b5b36a9d544269
LeoSanta183 <santazleo2004@gmail.com>
2024-04-21 11:16:34 -0400

    Added timer trigger, added enemy count in hud, made enemies a little faster

--- Assets/Script/ObjectDespawner.cs
+++ Assets/Script/ObjectDespawner.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ObjectDespawner : MonoBehaviour
 {
@@ -8,13 +10,18 @@ public class ObjectDespawner : MonoBehaviour
     public int totalEnemiesRequired = 2;
     private int currentEnemyCount = 0;
     AudioManager audioManager;
+    public TextMeshProUGUI enemyText;
 
     void Start()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
 
-   
+    void Update()
+    {
+        enemyText.SetText(currentEnemyCount + " / " + totalEnemiesRequired);
+    }
+
     public void EnemyDestroyed()
     {
         
--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -6,8 +6,10 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     private GameManager gameManager;
+    public GameObject wall;
     public GameObject coinPrefab;
     public Transform coinSpawn;
+    bool countdownStarted = false;
     [SerializeField] TextMeshProUGUI timerText;
     bool coinSpawned = false;
     [SerializeField] float remainingTime;
@@ -15,28 +17,43 @@ public class Timer : MonoBehaviour
     void Start()
     {
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        wall.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (remainingTime > 0)
+        if(countdownStarted)
         {
-            remainingTime -= Time.deltaTime;
-        }
-        else if (remainingTime < 0)
-        {
-            remainingTime = 0;
-        }
-        else if (remainingTime <= 0 && !coinSpawned)
-        {
-            gameManager.CancelInvoke("SpawnEnemy");
-            Instantiate(coinPrefab, coinSpawn.position, Quaternion.identity);
-            coinSpawned = true; // Set the flag to true so that coin is spawned only once
-        }
+            if (remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+            }
+            else if (remainingTime < 0)
+            {
+                remainingTime = 0;
+            }
+            else if (remainingTime <= 0 && !coinSpawned)
+            {
+                gameManager.CancelInvoke("SpawnEnemy");
+                Instantiate(coinPrefab, coinSpawn.position, Quaternion.identity);
+                coinSpawned = true; // Set the flag to true so that coin is spawned only once
+            }
 
-        int minutes = Mathf.FloorToInt(remainingTime / 60);
-        int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            int minutes = Mathf.FloorToInt(remainingTime / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !countdownStarted)
+        {
+            countdownStarted = true;
+            wall.SetActive(true);
+        }
+    }
+
 }
